b8de5aa141a18df31e0f3921df47d2f7799442aa
lisnote <didongxiaoli@gmail.com>
2022-02-23 23:35:43 +0800

    custom domain

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ gitblog = {
 		// 一般不需要修改,用于自定义笔记位置
 		"articles": "https://api.github.com/repos/{username}/{username}.github.io/contents/articles?ref=dev",
 		// 一般不需要修改,用于自定义笔记位置
-		"article": "https://{username}.github.io/articles/{article}",
+		"article": "{protocol}//{host}/articles/{article}",
 		// 一般不需要修改,用于自定义背景位置,例如jsdelivr加速,第三方随机图片api等
 		"background": "https://{username}.github.io/articles/assets/{article}/background.jpg",
 		// OAuth Apps,用于开启评论区及提高可访问性,申请地址 https://github.com/settings/developers 填写格式 clientID:clientSecret
--- js/gitblog.js
+++ js/gitblog.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 	switch (gitblog.api) {
 		case "github":
 			github();
@@ -15,10 +15,10 @@
 			console.log("username未设置,从链接中获取到username为", githubAPI.username);
 		};
 		githubAPI.articles = githubAPI.articles.replace(/{username}/g, githubAPI.username);
-		githubAPI.article = githubAPI.article.replace(/{username}/g, githubAPI.username);
+		githubAPI.article = githubAPI.article.replace(/{protocol}|{host}/g, data => data == "{host}" ? location.host : location.protocol);
 		githubAPI.background = githubAPI.article.replace(/{username}/g, githubAPI.username)
 		// 将articles下的文件名和文件夹名转换为数组保存在gitblog.articles
-		gitblog.getArticles = function() {
+		gitblog.getArticles = function () {
 			let articles = [];
 			$.ajax({
 				url: githubAPI.articles,
@@ -26,7 +26,7 @@
 				headers: {
 					authorization: "Basic " + btoa(githubAPI.clientID + ":" + githubAPI.clientSecret),
 				},
-				success: function(result) {
+				success: function (result) {
 					for (let i = 0; i < result.length; i++) {
 						if (result[i].name == "assets" || result[i].name == "index.html") continue;
 						articles[articles.length] = result[i].name;
@@ -36,7 +36,7 @@
 			return articles;
 		}
 		// 返回String类型的文章内容
-		gitblog.getArticle = function(article) {
+		gitblog.getArticle = function (article) {
 			let articleUrl = githubAPI.article.replace(/{article}/g, article);
 			return $.ajax({
 				async: false,
@@ -44,7 +44,7 @@
 			}).responseText;
 		}
 		// 获取背景 返回背景的链接
-		gitblog.getBackground = function(article) {
+		gitblog.getBackground = function (article) {
 			return githubAPI.background.replace(/{article}/g, article);
 		}
 	}
