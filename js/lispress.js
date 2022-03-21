63d5fcea46b037113aed3cdf289d75a6aade5217
lisnote <didongxiaoli@gmail.com>
2022-03-22 01:41:06 +0800

    pre filter before sort

--- js/lispress.js
+++ js/lispress.js
@@ -73,7 +73,10 @@
 				},
 				async: false,
 				success(result) {
-					result.items.forEach(element => {
+					result = result.items.filter(element => {
+						return !/articles\/.*\//.test(element.path)
+					})
+					result.forEach(element => {
 						try {
 							dateMap[element.name] = Number(element.text_matches[0].fragment
 								.match(/\d{4}(-\d{2}){2} \d{2}(:\d{2}){2}/)[0].replace(/-| |:/g, ""))
