bbbdd4ee0af48609dddbd7eedf71e70e44d86b2b
lisnote <didongxiaoli@gmail.com>
2022-03-22 19:51:39 +0800

    optimize structure

--- config.js
+++ config.js
@@ -1,30 +1,25 @@
-let github = {
+let githubConfig = {
 	// 自定义域名必须填写username
 	username: "",
-	// OAuth Apps,用于开启评论区及提高可访问性,申请地址 https://github.com/settings/developers 填写格式 clientID:clientSecret
+	// OAuth Apps,用于开启评论区及提高可访问性,申请地址 https://github.com/settings/developers
 	clientID: "de3105a8fb15edf7ca19",
 	clientSecret: "715c4a9cfe522069f914ff3f839119e6c4ae73a8",
-	// 一般不需要修改,用于自定义笔记位置
-	articles: "https://api.github.com/repos/{username}/{username}.github.io/contents/articles",
-	// 一般不需要修改,用于自定义笔记位置
-	article: "{protocol}//{host}/articles/{article}",
-	// 一般不需要修改,用于自定义背景位置,例如jsdelivr加速,第三方随机图片api等
-	background: "{protocol}//{host}/articles/assets/{article}/background.jpg",
 }
 
 // 一般不需要修改,用于自定义服务器
 var lispress = {
-	// 根据此api的名称选择进行处理的方法
-	api: "github",
-	// 传入处理方法所需要的配置信息
-	config: github,
-	// 自定义时请实现以下三个方法
-	// 返回一个字符串数组,包含除assets和index.html外,所有的articles子文件名和子文件夹名
+	// 负责实现lispress的其他字段和方法
+	impl: githubImpl,
+	// 所有的文章
 	articles: [],
+	// param 查找的关键字
+	// return string数组,包含符合查找条件的文章
+	getSearchArticles: null,
 	// param string类型的文章名
 	// return string类型的markdown直链
 	getArticle: null,
 	// param string类型文章/文件夹名
 	// return string类型的background直链
-	getBackground: null
+	getBackground: null,
 }
+lispress.impl()
--- js/lispress.js
+++ js/lispress.js
@@ -1,95 +1,89 @@
-(function () {
-	switch (lispress.api) {
-		case "github":
-			github();
-			break;
-		default:
-			github();
-	}
+function githubImpl() {
+    let githubAPI = githubConfig;
+    // 获取username,若获取失败则使用lisnote作为用户名
+    if (githubAPI.username == "") {
+        try {
+            githubAPI.username = (/.*\/(.*)\.github\.io.*/.exec(location.href)[1])
+        } catch (error) {
+            console.error("Failed to obtain username,have you set the github.username in config.js?");
+            // 使用开发者数据
+            githubAPI.username = "lisnote"
+        }
+    };
+    // 准备常用数据
+    githubAPI.articles = `https://api.github.com/repos/${githubAPI.username}/${githubAPI.username}.github.io/contents/articles`
+    githubAPI.article = `${location.protocol}//${location.host}/articles/{article}`
+    githubAPI.background = `${location.protocol}//${location.host}/articles/assets/{article}/background.jpg`
+    githubAPI.avatar = `https://avatars.githubusercontent.com/${githubAPI.username}`
+    // 实现lispress对象的四个方法
+    lispress.articles = articlesDecorator();
+    lispress.getSearchArticles;
+    lispress.getArticle = article => githubAPI.article.replace(/{article}/g, article);
+    lispress.getBackground = article => githubAPI.background.replace(/{article}/g, article);
+    // 替换id为avatar的元素的src属性值为githubAPI.avatar
+    $(() => {
+        $("#avatar").attr("src", githubAPI.avatar)
+    })
 
-	function github() {
-		let githubAPI = lispress.config;
-		// 获取username,若获取失败则使用lisnote作为用户名
-		if (githubAPI.username == "") {
-			try {
-				githubAPI.username = (/.*\/(.*)\.github\.io.*/.exec(location.href)[1])
-			} catch (error) {
-				console.error("Failed to obtain username,have you set the github.username in config.js?");
-				// 使用开发者数据
-				githubAPI.username = "lisnote"
-			}
-		};
-		// 准备常用数据
-		githubAPI.articles = `https://api.github.com/repos/${githubAPI.username}/${githubAPI.username}.github.io/contents/articles`
-		githubAPI.article = `${location.protocol}//${location.host}/articles/{article}`
-		githubAPI.background = `${location.protocol}//${location.host}/articles/assets/{article}/background.jpg`
-		githubAPI.avatar = `https://avatars.githubusercontent.com/${githubAPI.username}`
-		// 实现lispress要求的三个方法
-		lispress.articles = articlesDecorator();
-		lispress.getArticle = article => githubAPI.article.replace(/{article}/g, article);
-		lispress.getBackground = article => githubAPI.background.replace(/{article}/g, article);
-		// 替换id为avatar的元素的src属性值为githubAPI.avatar
-		$(() => {
-			$("#avatar").attr("src", githubAPI.avatar)
-		})
+    // 解析articles文件目录为数组并返回,忽略assets/和index.html
+    function articlesDecorator() {
+        let articles = [];
+        if (location.pathname != "/") {
+            return articles;
+        }
+        articles = getArticles();
+        sortArticles(articles);
+        return articles;
+    }
+    // 获取文章
+    function getArticles() {
+        let articles = [];
+        if (location.pathname == "/") {
+            $.ajax({
+                url: githubAPI.articles,
+                async: false,
+                headers: {
+                    authorization: "Basic " + btoa(githubAPI.clientID + ":" + githubAPI.clientSecret),
+                },
+                success: function (result) {
+                    for (let i = 0; i < result.length; i++) {
+                        if (result[i].name == "assets" || result[i].name == "index.html") continue;
+                        articles.push(result[i].name);
+                    }
+                }
+            })
+        }
+        return articles;
+    }
+    // 对传入文章进行排序
+    function sortArticles(articles) {
+        let dateMap = {};
+        $.get({
+            url: `https://api.github.com/search/code?q=date%20in:file%20user:${githubAPI.username}%20path:articles/%20language:markdown`,
+            headers: {
+                "Accept": "application/vnd.github.v3.text-match+json",
+            },
+            async: false,
+            success(result) {
+                result = result.items.filter(element => {
+                    return !/articles\/.*\//.test(element.path)
+                })
+                result.forEach(element => {
+                    try {
+                        dateMap[element.name] = Number(element.text_matches[0].fragment
+                            .match(/\d{4}(-\d{2}){2} \d{2}(:\d{2}){2}/)[0].replace(/-| |:/g, ""))
+                    } catch {
+                        dateMap[element.name] = 0
+                    }
+                });
+            }
+        })
+        articles.sort((x, y) => {
+            return dateMap[y] - dateMap[x];
+        })
+    }
+}
 
-		// 解析articles文件目录为数组并返回,忽略assets/和index.html
-		function articlesDecorator() {
-			let articles = [];
-			if (location.pathname != "/") {
-				return articles;
-			}
-			articles = getArticles();
-			sortArticles(articles);
-			return articles;
-		}
-		// 获取文章
-		function getArticles() {
-			let articles = [];
-			if (location.pathname == "/") {
-				$.ajax({
-					url: githubAPI.articles,
-					async: false,
-					headers: {
-						authorization: "Basic " + btoa(githubAPI.clientID + ":" + githubAPI.clientSecret),
-					},
-					success: function (result) {
-						for (let i = 0; i < result.length; i++) {
-							if (result[i].name == "assets" || result[i].name == "index.html") continue;
-							articles.push(result[i].name);
-						}
-					}
-				})
-			}
-			return articles;
-		}
-		// 获取日期映射dateMap
-		function sortArticles(articles) {
-			let dateMap = {};
-			$.get({
-				url: `https://api.github.com/search/code?q=date%20in:file%20user:${githubAPI.username}%20path:articles/%20language:markdown`,
-				headers: {
-					"Accept": "application/vnd.github.v3.text-match+json",
-				},
-				async: false,
-				success(result) {
-					result = result.items.filter(element => {
-						return !/articles\/.*\//.test(element.path)
-					})
-					result.forEach(element => {
-						try {
-							dateMap[element.name] = Number(element.text_matches[0].fragment
-								.match(/\d{4}(-\d{2}){2} \d{2}(:\d{2}){2}/)[0].replace(/-| |:/g, ""))
-						} catch {
-							dateMap[element.name] = 0
-						}
-					});
-				}
-			})
-			// 根据日期排序文章
-			articles.sort((x, y) => {
-				return dateMap[y] - dateMap[x];
-			})
-		}
-	}
-}())
+function customImpl() {
+    
+}
